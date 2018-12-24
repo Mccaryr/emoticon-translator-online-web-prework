22a9897b7cd495d6647d8d69de9ba07edf5aa8a1
Robert McCary <robert.mccary@yahoo.com>
2018-12-24 19:16:10 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,6 +3,11 @@ require 'emoticons.yml'
 
 def load_library(file_path)
 lib = {"get_translation" => {}, "get_emoticon" => {} }
+YAML.load_file(file_path).each do |meaning, array|
+  english, japanese = array
+  library["get_emoticon"][english] = japanese
+  library["get_translationi"][japanese] = meaning
+  end
 end
 
 def get_japanese_emoticon
